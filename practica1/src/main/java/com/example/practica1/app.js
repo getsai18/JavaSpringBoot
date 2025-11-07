a71a08dd0aea02d4f7bbfaf95a89edb8dba96cd5
getsai18 <20243ds167@utez.edu.mx>
2025-11-07 11:49:36 -0600

    Added keyDown and KeyUp

--- practica1/src/main/java/com/example/practica1/app.js
+++ practica1/src/main/java/com/example/practica1/app.js
@@ -1,15 +1,44 @@
 const formulario = document.querySelector(".formulario");
-
-formulario.addEventListener("submit", (e) => {
-
-    const elementos = document.querySelectorAll(".campoObligatorio");
+ const elementos = document.querySelectorAll(".campoObligatorio");
     const mostrarError = document.querySelector(".mostrarError");
 
+formulario.addEventListener("submit", (e) => {
+    let hayError = false
+    mostrarError.innerHtml="";
+
     for(let elemento of elementos){
-        if(elemento.value === ""){
+        if(elemento.value.trim() === ""){
+
+            hayError = true;
+            elemento.classList.remove("invalido");
+            elemento.classList.add("valido");
             e.preventDefault();
             mostrarError.innerHtml="<p>Falta llenar campos</p>";
+        }else{
+            elemento.classList.remove("valido");
+            elemento.classList.add("invalido");
         }
 
+
+
     }
+
+
+    if (hayError) {
+        e.preventDefault(); // no envía el formulario
+        mostrarError.innerHTML = "<p style='color:red;'>Faltan campos por llenar</p>";
+    }
+});
+
+
+elementos.forEach((campo) => {
+    campo.addEventListener("keyup", () => {
+        if (campo.value.trim() === "") {
+            campo.classList.remove("valido");
+            campo.classList.add("invalido");
+        } else {
+            campo.classList.remove("invalido");
+            campo.classList.add("valido");
+        }
+    });
 });
